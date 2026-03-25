c1b4680d356e367d8bda2859df23710444eb19c9
chrisdavis2110 <chrisdavis2110@gmail.com>
2026-03-24 20:59:23 -0700

    updated to 0.3.2

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ MIT License
 Main entry point for the MeshCore decoder Python port
 """
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 # Main decoder
 from src.decoder.packet_decoder import MeshCorePacketDecoder
